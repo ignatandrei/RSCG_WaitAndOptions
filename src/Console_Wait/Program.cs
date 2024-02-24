5c1aa4a2b042b8e442ad389e357b7dc7f0fb31fb
Andrei Ignat <ignatandrei@yahoo.com>
2024-02-24 19:31:47 +0200

    Add NuGet package push step and update project versions

--- src/Console_Wait/Program.cs
+++ src/Console_Wait/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine(RSCG_Wait.MyGeneratedCode.DateStart);
 Console.WriteLine(RSCG_Wait.MyGeneratedCode.SecondsToWait);
 Console.WriteLine(RSCG_Wait.MyGeneratedCode.DateEnd);
+Console.WriteLine(RSCG_Wait.OptionsFromBuild.build_property_projectdir);
